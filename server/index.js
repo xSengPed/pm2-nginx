a17024b22b232af0b688378ca5c6229132b18ba6
KalimaPz <kalima007c@gmail.com>
2020-04-15 23:12:28 +0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+let express = require("express");
+let app = express();
+let PORT = 3000;
+let persons = [
+  {
+    id: 0,
+    name: "Donnukrit Satirakul",
+    age: 18,
+    province: "Nakorn Sri Thammarat",
+  },
+  {
+    id: 1,
+    name: "Jonh Doe",
+    age: 21,
+    province: "London",
+  },
+];
+
+app.get("/", (req, res) => {
+  console.log(persons);
+  res.json(persons);
+});
+
+app.get("/crash", (req, res) => {
+  console.log("Crashed");
+  res.send("Crashed");
+  process.exit(1);
+});
+app.listen(PORT, () => console.log("SERVER HAS BEEN RUN ON PORT", PORT));
